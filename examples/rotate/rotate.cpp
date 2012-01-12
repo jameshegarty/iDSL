b5def01fccdc0bfaf4c3bbb855ffd3a7f0a252ea
James Hegarty <jameshegarty@gmail.com>
2012-01-11 18:00:55 -0800

    working rotation

--- examples/rotate/rotate.cpp
+++ examples/rotate/rotate.cpp
@@ -1,27 +1,46 @@
 #include "../util.h"
 #include <math.h>
 #include <stdio.h>
+#include <assert.h>
 
-unsigned char sample(int width, int height, float x, float y, unsigned char* in){
+unsigned char sampleBilinear(int width, int height, float x, float y, unsigned char* in){
   if(x < 0.f){return 0;}
   if(y < 0.f){return 0;}
   if(x > float(width)){return 0;}
   if(y > float(height)){return 0;}
-  float aa = in[int(y)*width+int(x)];
-  float ba = in[int(y)*width+int(x)+1];
-  float ab = in[(int(y)+1)*width+int(x)];
-  float bb = in[(int(y)+1)*width+int(x)+1];
+
+  int ix = x;
+  int iy = y;
+
+  float aa = in[iy*width+ix];
+  float ba = in[iy*width+ix+1];
+  float ab = in[(iy+1)*width+ix];
+  float bb = in[(iy+1)*width+ix+1];
   float rx = x-floor(x);
-  float rxi = floor(x)-x; // 1-rx
+  float rxi = 1.f-rx;
   float ry = y-floor(y);
-  float ryi = floor(y)-y; // 1-ry
+  float ryi = 1.f-ry; // 1-ry
+  assert(rx >= 0.f);assert(rx <=1.f);assert(rxi >=0.f);assert(rxi <= 1.f);
   float resa = aa*rxi+ba*rx;
   float resb = ab*rxi+bb*rx;
+
   return (unsigned char)(resa*ryi+resb*ry);
 }
 
+unsigned char sampleNearest(int width, int height, float x, float y, unsigned char* in){
+  if(x < 0.f){return 0;}
+  if(y < 0.f){return 0;}
+  if(x > float(width)){return 0;}
+  if(y > float(height)){return 0;}
+
+  int ix = x;
+  int iy = y;
+
+  return in[iy*width+ix];
+}
+
 void rotate(int width, int height, unsigned char* in, unsigned char* out){
-  float degrees = 42;
+  float degrees = 0.20f;
 
   float rotMatrix11 = cos(degrees); float rotMatrix12 = -sin(degrees);
   float rotMatrix21 = sin(degrees); float rotMatrix22 = cos(degrees);
@@ -31,7 +50,7 @@ void rotate(int width, int height, unsigned char* in, unsigned char* out){
       float sx = rotMatrix11*x + rotMatrix12*y;
       float sy = rotMatrix21*x + rotMatrix22*y;
 
-      out[y*width+x] = sample( width, height, sx, sy, in );
+      out[y*width+x] = sampleBilinear( width, height, sx, sy, in );
     }
   }
 }
