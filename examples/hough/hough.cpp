928153dc49b28333a9c9b1f078474a58f8c488f4
James Hegarty <jameshegarty@gmail.com>
2012-01-19 17:36:13 -0800

    working hough

--- examples/hough/hough.cpp
+++ examples/hough/hough.cpp
@@ -68,6 +68,51 @@ void findLines(
   }
 }
 
+void findTopNLines(
+  int *N,		   
+  int width,
+  int height,
+  int rSize,
+  int thetaSize,
+  float *hough,
+  int *rList,
+  int *thetaList,
+  float *valList){
+
+  float maxR = sqrt(width*width+height*height);
+
+  for( int i = 0; i < *N; i++ ){ valList[i] = 0; }
+
+  for( int r = 0; r<rSize; r++ ){
+    for(int theta = 0; theta < thetaSize; theta++){
+      float val = hough[theta*rSize+r];
+      
+      // find if we're larger than anything
+      int j = *N;
+      while(valList[j-1] < val && j > 0){j--;}
+      
+      // if so, push it on the stack
+      if(j < *N){
+
+	for( int t = *N-1; t > j; t-- ){
+	  valList[t] = valList[t-1];
+	  rList[t] = rList[t-1];
+	  thetaList[t] = thetaList[t-1];
+	}
+
+	valList[j] = val;
+	rList[j] = r * (maxR / float(rSize));
+	thetaList[j] = theta * (360.f / float (thetaSize));
+      }
+    }
+  }
+
+  // eliminate lines that are way worse than the best one
+  for( int i = 0; i < *N; i++){
+    if( valList[i] / valList[0] < 0.75f ){ *N = i; break; }
+  }
+}
+
 int main(int argc, char **argv){
 
   if(argc!=2){
@@ -101,13 +146,15 @@ int main(int argc, char **argv){
     houghOut);
   
   // find lines
-  std::vector<int> rList;
-  std::vector<int> thetaList;
+  int N = 16;
+  int *rList = new int[N];
+  int *thetaList = new int[N];
+  float *valList = new float[N];
 
-  findLines( width, height, rSize, thetaSize, houghOut, rList, thetaList );
+  findTopNLines( &N, width, height, rSize, thetaSize, houghOut, rList, thetaList, valList );
 
-  for(unsigned int i=0; i<rList.size(); i++){
-    printf("%d %d\n",rList[i],thetaList[i]);
+  for(int i=0; i<N; i++){
+    printf("%d %d %f\n",rList[i],thetaList[i], valList[i]);
   }
 
   // write out hough
@@ -124,8 +171,8 @@ int main(int argc, char **argv){
 
   float maxR = sqrt(width*width+height*height);
 
-  for(unsigned int i=0; i<rList.size(); i++){
-    int j = thetaList[i]*rSize+(rList[i]*float(rSize)/maxR);
+  for( int i=0; i<N; i++){
+    int j = (thetaList[i]*float(thetaSize)/360.f)*rSize+(rList[i]*float(rSize)/maxR);
     houghTemp[j*3] = 255;
     houghTemp[j*3+1] = 0;
     houghTemp[j*3+2] = 0;
@@ -135,7 +182,7 @@ int main(int argc, char **argv){
 
   // draw lines
 
-  for(unsigned int i=0; i<rList.size(); i++){
+  for( int i=0; i<N; i++){
     // find intercepts
     int x[2];
     int y[2];
@@ -163,6 +210,9 @@ int main(int argc, char **argv){
 
   res = saveImage("in.bmp", width, height, channels, data);
 
+  delete[] rList;
+  delete[] thetaList;
+  delete[] valList;
   delete[] houghOut;
   delete[] canny;
 
