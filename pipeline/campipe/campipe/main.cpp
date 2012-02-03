0fc687fb6fcb18ef7d0cad1fbe5b51ae1eac27b5
Phaedon Sinis <phaedonsinis@gmail.com>
2012-02-02 16:13:02 -0800

    working on color form...

--- pipeline/campipe/campipe/main.cpp
+++ pipeline/campipe/campipe/main.cpp
@@ -11,6 +11,59 @@
 
 static int counter = 0;
 
+void *colordarkenstage (void *args);
+
+void *colordarkenstage (void *args) {
+    Image *img = (Image *)args;
+
+    int w = img->width;
+    int h = img->height * 3; // ARGH ARGH ARGH
+    
+    // bmps pad each row to be a multiple of 4 bytes
+    int padding = 4 - (w * 3 % 4); // we always write 3 channels
+    padding = padding == 4 ? 0 : padding;
+    
+    //printf("padding: %d\n", padding);
+    //printf("width: %d   height: %d\n", w, h);
+    
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j+=3) {
+            
+            if (i == h - 1 && j == w - 1) break;
+            
+            // grab existing colors
+            unsigned char r = sampleNearest(w, h, j, i, img->data);
+            unsigned char g = sampleNearest(w, h, j+1, i, img->data);
+            unsigned char b = sampleNearest(w, h, j+2, i, img->data);
+
+            int wcolor = 255 * (float)j / (float)(w);
+            int hcolor = 255 * (float)i / (float)(img->height);
+            
+            int redIndex = i*w + j+0;
+            int greenIndex = i*w + j+1;
+            int blueIndex = i*w + j+2;
+            if (i%3 == 1) {
+                redIndex = i*w + j+2;
+                greenIndex = i*w + j+0;
+                blueIndex = i*w + j+1;
+
+            } else if (i%3 == 2) {
+                redIndex = i*w + j+1;
+                greenIndex = i*w + j+2;
+                blueIndex = i*w + j+0;
+            }
+            
+            img->data[ redIndex ] = r; 
+            img->data[ greenIndex ] = g;
+            img->data[ blueIndex ] = b;
+
+        }
+    }
+    
+    return img;
+}
+
+
 void *finalstage (void *args);
 
 void *finalstage (void *args) {
@@ -47,7 +100,8 @@ int main (int argc, const char * argv[])
     Pipeline p(5, stages);
     
     
-    // Give it the "final-stage" code which simply saves to disk:
+    // Give it some code that actually does stuff:
+    p.setStageCode(2, &colordarkenstage);
     p.setStageCode(4, &finalstage);
     
     // Hook up the sensor, launch the simulation!
