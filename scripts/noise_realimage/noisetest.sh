17a119736c8e14043a8dfd69935113b4371e4883
James Hegarty <jhegarty@thirtytwo.Stanford.EDU>
2012-02-28 18:22:33 -0800

    fix stuff that gdb doesn't like

--- scripts/noise_realimage/noisetest.sh
+++ scripts/noise_realimage/noisetest.sh
@@ -8,11 +8,15 @@ rm res_lk3.txt
 
 for (( i=0; i<100; i+=5 ))
 do
-    ${IMAGESTACK} -load m1.png -translate 2 2 -noise 0 0.0`printf "%02d" $i` -save frame2.ppm
-    
-    ${EXAMPLES}motionestimation/motionestimation ./m1.bmp ./frame2.ppm 3 3
-    ${EXAMPLES}diff/diff --L2 vectors.bmp ref_w3.bmp >> res_me.txt
+    ${IMAGESTACK} -load m1.png -translate 2 2 -noise 0 0.0`printf "%02d" $i` -save ./temp/frame2_`printf "%02d" $i`.ppm &
+done
 
-    ${EXAMPLES}lucaskanade/lucaskanade ./m1.bmp ./frame2.ppm 9 3
-    ${EXAMPLES}diff/diff --L2 vectors.bmp ref_w3.bmp >> res_lk3.txt
+for (( i=0; i<100; i+=5 ))
+do
+    ${EXAMPLES}motionestimation/motionestimation ./m1.bmp ./temp/frame2_`printf "%02d" $i`.ppm 3 3 && ${EXAMPLES}diff/diff --L2 ./temp/vectors.bmp ref_w3.bmp >> ./results/res_me.txt &
+done
+
+for (( i=0; i<100; i+=5 ))
+do
+    ${EXAMPLES}lucaskanade/lucaskanade ./m1.bmp ./temp/frame2_`printf "%02d" $i`.ppm 9 3 && ${EXAMPLES}diff/diff --L2 ./temp/vectors.bmp ref_w3.bmp >> ./results/res_lk3.txt &
 done
