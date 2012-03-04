dd4b61ca2f00a1a397a8e066923b4ea0b3b957bb
James Hegarty <jhegarty@thirtytwo.Stanford.EDU>
2012-03-03 23:08:41 -0800

    denoise script

--- scripts/denoise/denoise.sh
+++ scripts/denoise/denoise.sh
@@ -6,8 +6,8 @@ EXAMPLES="../../examples/"
 rm ./results/*
 rm ./temp/*
 
-for (( i=0; i<1; i+=1 ))
+for (( i=1; i<6; i+=1 ))
 do
     output="./temp/vectors_`printf "%03d" $i`.bmp"
-    ${EXAMPLES}lucaskanade/lucaskanade ./frame`printf "%03d" $i`.bmp ./frame000.bmp $output 6 3 2 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp &
+    ${EXAMPLES}lucaskanade/lucaskanade ./frame`printf "%03d" $i`.bmp ./frame000.bmp $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp &
 done
