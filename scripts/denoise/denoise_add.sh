ec70ec69cae9be00ee2a7d35b3621e2a45272699
James Hegarty <jhegarty@thirtytwo.Stanford.EDU>
2012-03-04 00:29:27 -0800

    deblur

--- scripts/denoise/denoise_add.sh
+++ scripts/denoise/denoise_add.sh
@@ -9,8 +9,9 @@ rm command.txt
 
 for (( i=1; i<14; i+=1 ))
 do
+    j=i-1
     output="./temp/vectors_`printf "%03d" $i`.bmp"
-    ${EXAMPLES}lucaskanade/lucaskanade ./frame000.bmp ./frame`printf "%03d" $i`.bmp  $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp && convert ${output}.back.bmp ${output}.back.png &
+    ${EXAMPLES}lucaskanade/lucaskanade ./frame`printf "%03d" $j`.bmp ./frame`printf "%03d" $i`.bmp  $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp && convert ${output}.back.bmp ${output}.back.png &
 done
 
 echo -ne "ImageStack -load ./temp/vectors_001.bmp.back.png " >> command.txt
