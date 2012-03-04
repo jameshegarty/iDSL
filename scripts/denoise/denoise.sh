6d8a6dc83bef23df3061d94e355d4efff5c14d9e
James Hegarty <jameshegarty@gmail.com>
2012-03-03 23:30:06 -0800

    fixed

--- scripts/denoise/denoise.sh
+++ scripts/denoise/denoise.sh
@@ -9,5 +9,5 @@ rm ./temp/*
 for (( i=1; i<6; i+=1 ))
 do
     output="./temp/vectors_`printf "%03d" $i`.bmp"
-    ${EXAMPLES}lucaskanade/lucaskanade ./frame`printf "%03d" $i`.bmp ./frame000.bmp $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp &
+    ${EXAMPLES}lucaskanade/lucaskanade ./frame000.bmp ./frame`printf "%03d" $i`.bmp  $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp $output ${output}.back.bmp &
 done
