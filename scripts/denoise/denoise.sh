a42d9d2de7b122d8e57c3b06526ec98dad45a8f2
James Hegarty <jhegarty@thirtytwo.Stanford.EDU>
2012-03-04 18:46:02 -0800

    flo

--- scripts/denoise/denoise.sh
+++ scripts/denoise/denoise.sh
@@ -17,9 +17,9 @@ do
     ${EXAMPLES}lucaskanade/lucaskanade ./${anim}000.bmp ./${anim}`printf "%03d" $i`.bmp  $output 6 3 4 && ${EXAMPLES}backAdvect/backAdvect ./${anim}`printf "%03d" $i`.bmp $output ${output}.back.bmp && convert ${output}.back.bmp ${output}.back.png &
 done
 
-echo -ne "ImageStack -load ./temp/vectors_001.bmp.back.png " >> command.txt
+echo -ne "ImageStack -load ./temp/vectors_001.flo.back.png " >> command.txt
 
 for (( i=2; i<$frames; i+=1 ))
 do
-  echo -ne "-load ./temp/vectors_`printf "%03d" $i`.bmp.back.png -add " >> command.txt
+  echo -ne "-load ./temp/vectors_`printf "%03d" $i`.flo.back.png -add " >> command.txt
 done
--- scripts/denoise/denoise_add.sh
+++ scripts/denoise/denoise_add.sh
@@ -7,36 +7,36 @@ rm ./results/*
 rm ./temp/*
 rm command.txt
 
-if [ $# -ne 1 ]; then echo "Usage: denoise_add.sh animfilenameroot"; exit; fi
+if [ $# -ne 2 ]; then echo "Usage: denoise.sh animfilenameroot frames"; exit; fi
 anim=$1
+frames=$2
 
-for (( i=1; i<14; i+=1 ))
+for (( i=1; i<$frames; i+=1 ))
 do
     j=`expr $i - 1`
     output="./temp/vectors_`printf "%03d" $i`.flo"
-    ${EXAMPLES}lucaskanade/lucaskanade ./frame`printf "%03d" $j`.bmp ./frame`printf "%03d" $i`.bmp  $output 6 3 4 &
+    ${EXAMPLES}lucaskanade/lucaskanade ./${anim}`printf "%03d" $j`.bmp ./${anim}`printf "%03d" $i`.bmp  $output 6 3 4 &
 done
 
 wait
-echo "awesome"
 
-for (( i=1; i<14; i+=1 ))
+for (( i=2; i<$frames; i+=1 ))
 do
     j=`expr $i - 1`
     output="./temp/vectors_`printf "%03d" $i`.flo"
     outputj="./temp/vectors_`printf "%03d" $j`.flo"
-    ImageStack -load ${outputj}.flo -load ${output}.flo -add -save  ${output}.add.flo
+    ImageStack -load ${outputj} -load ${output} -add -save  ${output}.add.flo
 done
 
-for (( i=1; i<14; i+=1 ))
+for (( i=1; i<$frames; i+=1 ))
 do
   output="./temp/vectors_`printf "%03d" $i`.flo"
-  ${EXAMPLES}backAdvect/backAdvect ./frame`printf "%03d" $i`.bmp ${output}.add.flo ${output}.back.bmp && convert ${output}.back.bmp ${output}.back.png &
+  ${EXAMPLES}backAdvect/backAdvect ./${anim}`printf "%03d" $i`.bmp ${output}.add.flo ${output}.back.bmp && convert ${output}.back.bmp ${output}.back.png &
 done
 
-echo -ne "ImageStack -load ./temp/vectors_001.bmp.back.png " >> command.txt
+echo -ne "ImageStack -load ./temp/vectors_001.flo.back.png " >> command.txt
 
-for (( i=2; i<14; i+=1 ))
+for (( i=2; i<$frames; i+=1 ))
 do
-  echo -ne "-load ./temp/vectors_`printf "%03d" $i`.bmp.back.png -add " >> command.txt
+  echo -ne "-load ./temp/vectors_`printf "%03d" $i`.flo.back.png -add " >> command.txt
 done
