940b0e811d4d454f23a4c299e39af8497fd8b063
Matthew Cong <matthew.d.cong@gmail.com>
2012-03-13 23:33:50 -0700

    Separate out threading. Rename folder for future struct of arrays implementation.

---
+++ examples/s3d/array_of_structs/hypterm_threaded.h
@@ -0,0 +1,36 @@
+#ifndef __HYPTERM_THREADED_H__
+#define __HYPTERM_THREADED_H__
+
+#include <cmath>
+#include "constants.h"
+#include "hypterm.h"
+#include "hypterm_ispc.h"
+
+struct thread_data{
+    int thread_id;
+    int n0;
+    int n1;
+    int n2;
+    int n0s;
+    int n1s;
+    int n2s;
+    int n0e;
+    int n1e;
+    int n2e;
+    int ng0;
+    int ng1;
+    int ng2;
+    double dx0;
+    double dx1;
+    double dx2;
+    int nspec;
+    double *pres;
+    double *cons;
+    double *flux;
+    int blocksize;
+};
+
+void *hypterm_threaded(void *threadarg);
+void *hypterm_threaded_ispc(void *threadarg);
+
+#endif
